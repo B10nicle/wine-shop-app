1ab4f5503d6573515c45c588685407d6f4337453
Rufat Yagofarov <rufat.yagofarov@yandex.ru>
2023-05-28 00:28:19 +0300

    add folders

---
+++ backend/wine-shop-main/src/test/java/ru/wine/shop/WineShopApplicationTest.java
@@ -0,0 +1,12 @@
+package ru.wine.shop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WineShopApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
