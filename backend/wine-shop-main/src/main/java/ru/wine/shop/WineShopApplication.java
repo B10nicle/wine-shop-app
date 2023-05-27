455c6b800d249ff14b55028bcff0d18d085dc41e
Rufat Yagofarov <rufat.yagofarov@yandex.ru>
2023-05-27 23:14:10 +0300

    SringBoot init

---
+++ backend/wine-shop-main/src/main/java/ru/wine/shop/WineShopApplication.java
@@ -0,0 +1,11 @@
+package ru.wine.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineShopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WineShopApplication.class);
+    }
+}
